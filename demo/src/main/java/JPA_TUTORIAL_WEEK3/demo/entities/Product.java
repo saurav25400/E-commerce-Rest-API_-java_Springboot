99ad8da81d804c10d39bfdf5d86686bb28928177
saurav <saurav25400@gmailcom>
2024-11-23 22:52:13 +0530

    first commit

---
+++ demo/src/main/java/JPA_TUTORIAL_WEEK3/demo/entities/Product.java
@@ -0,0 +1,54 @@
+package JPA_TUTORIAL_WEEK3.demo.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.cglib.core.Local;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@Table(name="product_table",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "title_price_unique", columnNames = {"title_x", "price"})
+        },
+        indexes = {
+                @Index(name="sku_index",columnList="sku")
+        }
+
+
+)
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false,length=20)
+    private String sku;
+
+    @Column(name="title_x")
+    private String title;
+
+
+    private BigDecimal price;
+
+    private Integer quantity;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
---
+++ demo/src/test/java/JPA_TUTORIAL_WEEK3/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package JPA_TUTORIAL_WEEK3.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
