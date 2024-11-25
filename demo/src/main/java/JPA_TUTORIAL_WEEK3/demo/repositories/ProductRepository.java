fb1f29716e8d1ccf8466277f666882d433c7d119
saurav <saurav25400@gmailcom>
2024-11-25 23:33:36 +0530

    custom query methods added

--- demo/src/main/java/JPA_TUTORIAL_WEEK3/demo/repositories/ProductRepository.java
+++ demo/src/main/java/JPA_TUTORIAL_WEEK3/demo/repositories/ProductRepository.java
@@ -3,7 +3,12 @@ package JPA_TUTORIAL_WEEK3.demo.repositories;
 import JPA_TUTORIAL_WEEK3.demo.entities.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<ProductEntity,Long> {
+    List<ProductEntity> findByTitle(String title);
 
 
+    ProductEntity findByPriceAndQuantity( BigDecimal price,Integer quantity);
 }
--- demo/src/test/java/JPA_TUTORIAL_WEEK3/demo/DemoApplicationTests.java
+++ demo/src/test/java/JPA_TUTORIAL_WEEK3/demo/DemoApplicationTests.java
@@ -1,13 +1,46 @@
 package JPA_TUTORIAL_WEEK3.demo;
 
+import JPA_TUTORIAL_WEEK3.demo.entities.ProductEntity;
+import JPA_TUTORIAL_WEEK3.demo.repositories.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 
 @SpringBootTest
 class DemoApplicationTests {
+	@Autowired
+	ProductRepository productRepository;
 
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void addProduct(){
+		ProductEntity productEntity=ProductEntity.builder().sku("nestle23").title("chocolate").price(BigDecimal.valueOf(125.78)).quantity(4).build();
+		ProductEntity savedEntity=productRepository.save(productEntity);
+		System.out.println(savedEntity);
+
+	}
+
+	@Test
+	void getRepository(){
+		List<ProductEntity> productEntity=productRepository.findByTitle("chocolate");
+		System.out.println(productEntity);
+
+	}
+
+	@Test
+	void getDataByPriceAndQuantity(){
+		ProductEntity allData=productRepository.findByPriceAndQuantity(BigDecimal.valueOf(23.45),4);
+		System.out.println(allData);
+
+	}
+
+
 
 }
